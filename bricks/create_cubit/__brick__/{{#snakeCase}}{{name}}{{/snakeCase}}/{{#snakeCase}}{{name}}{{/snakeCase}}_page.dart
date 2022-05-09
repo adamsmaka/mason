39d1dd4f2382901a0fb1453928b7292a18563a41
Adam Smaka <adam@smaka.dev>
2022-05-09 13:13:43 +0200

    Fix imports

--- bricks/create_cubit/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_page.dart
+++ bricks/create_cubit/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_page.dart
@@ -1,10 +1,13 @@
 {{> common_header.md }}
 
-import 'package:balansik/app/core/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:{{#snakeCase}}{{app_name}}{{/snakeCase}}/features/{{#snakeCase}}{{name}}{{/snakeCase}}/cubit/{{#snakeCase}}{{name}}_cubit{{/snakeCase}}.dart';
+import '../../app/core/enums.dart';
+import '../../app/core/injection_container.dart';
+import 'cubit/add_action_cubit.dart';
+
+// import 'package:{{#snakeCase}}{{app_name}}{{/snakeCase}}/features/{{#snakeCase}}{{name}}{{/snakeCase}}/cubit/{{#snakeCase}}{{name}}_cubit{{/snakeCase}}.dart';
 
 class {{#pascalCase}}{{name}}Page{{/pascalCase}} extends StatelessWidget {
   const {{#pascalCase}}{{name}}Page{{/pascalCase}}({Key? key}) : super(key: key);
